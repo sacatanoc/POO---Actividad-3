b47a5515ec21d6e2ba09e86a6eb33bcdedf70a44
Samuel Cataño C <Samuel Catao C@LAPTOP-H1JKSR5M>
2024-12-21 13:15:51 -0500

    Actividad 3 - Parte 1 - Ejercicio 6 - Efraín Oviedo - Capítulo 4 - Ejercicio propuesto 23

---
+++ src/main/java/com/mycompany/com/mycompany/parte_1_ejercicio_6_efrain_oviedo_capitulo_4_ejercicio_propuesto_23/ComMycompanyParte_1_ejercicio_6_efrain_oviedo_capitulo_4_ejercicio_propuesto_23.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.com.mycompany.parte_1_ejercicio_6_efrain_oviedo_capitulo_4_ejercicio_propuesto_23;
+
+/**
+ *
+ * @author Samuel Cataño C
+ */
+public class ComMycompanyParte_1_ejercicio_6_efrain_oviedo_capitulo_4_ejercicio_propuesto_23 {
+
+    public static void main(String[] args) {
+        Ventana_principal ventana = new Ventana_principal();
+        ventana.setVisible(true);
+    }
+}
