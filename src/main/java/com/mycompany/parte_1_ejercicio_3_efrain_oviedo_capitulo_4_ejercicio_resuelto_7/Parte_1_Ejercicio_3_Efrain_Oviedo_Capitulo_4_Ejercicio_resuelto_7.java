13c2f9b93d5cf8e2250c46e84bc00d3a7e4f5c89
Samuel Cataño C <Samuel Catao C@LAPTOP-H1JKSR5M>
2024-12-21 13:02:10 -0500

    Actividad 3 - Parte 1 - Ejercicio 3 - Efraín Oviedo - Capítulo 4 - Ejercicio resuelto 7

---
+++ src/main/java/com/mycompany/parte_1_ejercicio_3_efrain_oviedo_capitulo_4_ejercicio_resuelto_7/Parte_1_Ejercicio_3_Efrain_Oviedo_Capitulo_4_Ejercicio_resuelto_7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parte_1_ejercicio_3_efrain_oviedo_capitulo_4_ejercicio_resuelto_7;
+
+/**
+ *
+ * @author Samuel Cataño C
+ */
+public class Parte_1_Ejercicio_3_Efrain_Oviedo_Capitulo_4_Ejercicio_resuelto_7 {
+
+    public static void main(String[] args) {
+        Ventana_principal ventana = new Ventana_principal();
+        ventana.setVisible(true);
+    }
+}
